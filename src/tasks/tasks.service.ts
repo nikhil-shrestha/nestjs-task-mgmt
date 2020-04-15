a207a02a5d1e28227a9a97b522daea24b753f7be
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-15 08:38:19 +0545

    defining Task model

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './tasks.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
   getAllTasks() {
     return this.tasks;
