1d78a8d951150ecb49f76b97e76a857a059ec994
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-18 13:02:48 +0545

    creating a task (controller/service)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -8,5 +8,13 @@ export class TasksController {
   @Get()
   getAllTasks() {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ) {
+    return this.tasksService.createTask(title, description);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './tasks.model';
+import * as uuid from 'uuid/v1';
+import { Task, TasksStatus } from './tasks.model';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +8,17 @@ export class TasksService {
 
   getAllTasks() {
     return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TasksStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }
