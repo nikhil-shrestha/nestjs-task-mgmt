2bd94bc488a6a63574904479b9e9944a944f7712
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-19 09:46:10 +0545

    validation with AuthCredentialDto, password strength

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -6,7 +6,7 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
+  signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
     return this.authService.signUp(authCredentialsDto);
   }
 }
