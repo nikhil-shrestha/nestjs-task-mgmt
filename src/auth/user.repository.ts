d61785bb7d0785c2636ce590961b8421fafd8d11
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-19 09:39:11 +0545

    setting up auth & added signup

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,15 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    await user.save();
+  }
+}
